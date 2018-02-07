8dbcdf764a47af3cf81a0d2a5d23f04fb759a80c
Jorge Mauricio <jorge.mauricio@me.com>
2018-02-07 16:18:43 -0600

    agregar un color diferente a cada rango

--- algoritmos/generar_mapas.py
+++ algoritmos/generar_mapas.py
@@ -22,43 +22,45 @@ import math
 from api import claves
 
 def main():
-    print("Init")
-    FECHA_PRONOSTICO = descargarInfo()
-    mapasExtremos(FECHA_PRONOSTICO)
+	print("Init")
+	FECHA_PRONOSTICO = descargarInfo()
+	mapasExtremos(FECHA_PRONOSTICO)
 
 def descargarInfo():
-    # datos del servidor
-    serverInfo = claves()
-    # conexión al server
-    ftp = ftplib.FTP(serverInfo.ip)
-    # login al servidor
-    ftp.login(serverInfo.usr, serverInfo.pwd)
-    # arreglo para determinar fecha
-    arregloArchivos = []
-    arregloFechas = []
-    ftp.dir(arregloArchivos.append)
-    for archivo in arregloArchivos:
-        arregloArchivo = archivo.split()
-        arregloFechas.append(arregloArchivo[8])
-    FECHA_PRONOSTICO = arregloFechas[-1]
-    rutaPronostico = "data/{}".format(FECHA_PRONOSTICO)
-    ftp.cwd(FECHA_PRONOSTICO)
-    # validar la ruta para guardar los datos
-    if not os.path.exists(rutaPronostico):
-        os.mkdir(rutaPronostico)
-    else:
-        print("***** Carpeta ya existe")
-
-    # descarga de información
-    for i in range(1,6):
-        rutaArchivoRemoto = "d{}.txt".format(i)
-        rutaArchivoLocal = "{}/d{}.txt".format(rutaPronostico,i)
-        lf = open(rutaArchivoLocal, "wb")
-        ftp.retrbinary("RETR " + rutaArchivoRemoto, lf.write, 8*1024)
-        lf.close()
-    ftp.close()
-
-    return FECHA_PRONOSTICO
+	# datos del servidor
+	serverInfo = claves()
+	# conexión al server
+	ftp = ftplib.FTP(serverInfo.ip)
+	# login al servidor
+	ftp.login(serverInfo.usr, serverInfo.pwd)
+	# arreglo para determinar fecha
+	arregloArchivos = []
+	arregloFechas = []
+	ftp.dir(arregloArchivos.append)
+	for archivo in arregloArchivos:
+	    arregloArchivo = archivo.split()
+	    arregloFechas.append(arregloArchivo[8])
+	FECHA_PRONOSTICO = arregloFechas[-1]
+	rutaPronostico = "data/{}".format(FECHA_PRONOSTICO)
+	ftp.cwd(FECHA_PRONOSTICO)
+	# validar la ruta para guardar los datos
+	if not os.path.exists(rutaPronostico):
+		os.mkdir(rutaPronostico)
+	else:
+		print("***** Carpeta ya existe")
+		rutaArchivoPronostico = f"{rutaPronostico}/d1.txt"
+		if rutaArchivoPronostico:
+			print("Archivo d1.txt existe")
+		else:
+			# descarga de información
+			for i in range(1,6):
+				rutaArchivoRemoto = "d{}.txt".format(i)
+				rutaArchivoLocal = "{}/d{}.txt".format(rutaPronostico,i)
+				lf = open(rutaArchivoLocal, "wb")
+				ftp.retrbinary("RETR " + rutaArchivoRemoto, lf.write, 8*1024)
+				lf.close()
+			ftp.close()
+	return FECHA_PRONOSTICO
 
 def generarFechas(f):
 	"""
@@ -137,6 +139,59 @@ def generarTexto(f, k,vMn, vMx):
 	else:
 		pass
 
+def colorPuntoEnMapa(variable,rango):
+	"""
+	Función que permite generar el color del putno que se va a mostrar en el mapa
+	"""
+	if variable == "Rain":
+		if rango == "20/50":
+			return 'aqua'
+		elif rango == "50/70":
+			return 'azure'
+		elif rango == "70/150":
+			return 'darkblue'
+		elif rango == "150/300":
+			return 'tomato'
+		elif rango == "300/500":
+			return 'crimson'
+
+	if variable == "Tmax":
+		if rango == "30/35":
+			return 'coral'
+		elif rango == "35/40":
+			return 'orange'
+		elif rango == "40/45":
+			return 'crimson'
+		elif rango == "45/50":
+			return 'tomato'
+		elif rango == "50/60":
+			return 'maroon'
+
+	if variable == "Tmin":
+		if rango == "6/3":
+			return 'powderblue'
+		elif rango == "3/0":
+			return 'lightskyblue'
+		elif rango == "0/-3":
+			return 'dodgerblue'
+		elif rango == "-3/-6":
+			return 'steelblue'
+		elif rango == "-6/-9":
+			return 'darkblue'
+
+	if variable == "Windpro":
+		if rango == "62/74":
+			return 'gold'
+		elif rango == "75/88":
+			return 'sandybrown'
+		elif rango == "89/102":
+			return 'darksalmon'
+		elif rango == "103/117":
+			return 'darkorange'
+		elif rango == "118/150":
+			return 'maroon'
+
+
 def mapasExtremos(fp):
 	"""
 	Función que permite generar los mapas de eventos extremos
@@ -157,12 +212,15 @@ def mapasExtremos(fp):
 	LONG_MIN = -118.2360
 	LAT_MAX = 33.5791
 	LAT_MIN = 12.37
+
+	# ********** Path
 	path = "/Users/jorgemauricio/Documents/Research/alermapweb"
 	os.chdir(path)
 
 	# ********** dict de análisis
-	d = {"Rain" : ['20/50', '50/70', '70/150', '150/300', '300/500'], "Tmax":['30/35', '35/40', '40/45', '45/50', '50/60'], "Tmin" : ['-10/3'], "Windpro" : ['62/74', '75/88', '89/102', '103/117', '118/150']}
+	d = {"Rain" : ['20/50', '50/70', '70/150', '150/300', '300/500'], "Tmax":['30/35', '35/40', '40/45', '45/50', '50/60'], "Tmin" : ['6/3','3/0','0/-3','-3/-6','-6/-9'], "Windpro" : ['62/74', '75/88', '89/102', '103/117', '118/150']}
 
+	# ********** array colores
 	# generar fechas mediante función
 	arrayFechas = generarFechas(fechaPronostico)
 
@@ -193,7 +251,7 @@ def mapasExtremos(fp):
 				#%% set up plot
 				plt.clf()
 
-				fig = plt.figure(figsize=(12,8))
+				fig = plt.figure(figsize=(8,4))
 				m = Basemap(projection='mill',llcrnrlat=LAT_MIN,urcrnrlat=LAT_MAX,llcrnrlon=LONG_MIN,urcrnrlon=LONG_MAX,resolution='h')
 
 				# generar xp
@@ -209,7 +267,8 @@ def mapasExtremos(fp):
 				# m.bluemarble()
 
 				# gráficar puntos
-				m.scatter(xp, yp, latlon=True, s=3, marker='o', color='b', zorder=25, alpha=0.5)
+				colorTemporalDelPunto = colorPuntoEnMapa(key, i)
+				m.scatter(xp, yp, latlon=True, s=3, marker='o', color=colorTemporalDelPunto, zorder=25, alpha=0.5)
 
 				# titulo del mapa
 				tituloTemporalMapa = generarTexto(arrayFechas[j-1], key, vMin, vMax)
